4b73ea82d6999b80f8879a8486a51e6ea4044414
Iryna <nebesna1808@gmail.com>
2023-03-23 18:26:35 +1000

    second commit

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,17 +1,12 @@
-export const fetchImages = async (inputValue, pageNumber) => {
-    return await fetch(
-      `https://pixabay.com/api/?key=34605024-7c953be56e187075574c7e205=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNumber}`
-    )
-      .then(async response => {
-        if (!response.ok) {
-          if (response.status === 404) {
-            return [];
-          }
-          throw new Error(response.status);
-        }
-        return await response.json();
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
+import axios from 'axios';
+export { fetchImages };
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const KEY = '34605024-7c953be56e187075574c7e205';
+
+async function fetchImages(query, page, perPage) {
+  const response = await axios.get(
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+  );
+  return response;
+}
